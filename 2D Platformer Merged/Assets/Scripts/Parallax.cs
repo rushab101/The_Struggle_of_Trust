d6988c8ae786b5e3c828d61055c2ac9cf113b103
Uddin-Humaiyun-215492994 <46493951+Uddin-Humaiyun-215492994@users.noreply.github.com>
2020-05-03 13:28:13 -0400

    Added parallax effect bg and organized assets

---
+++ 2D Platformer Merged/Assets/Scripts/Parallax.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    private float length, startPos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start() {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate() {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float distance = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length) {
+            startPos += length;
+        }
+        else if (temp < startPos - length) {
+            startPos -= length;
+        }
+    }
+}
