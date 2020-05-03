2c645e34596cd07fa98ef25a8f11aab0b2042819
Uddin-Humaiyun-215492994 <46493951+Uddin-Humaiyun-215492994@users.noreply.github.com>
2020-05-03 01:31:08 -0400

    Update CombatDummyController.cs

--- 2D Platformer Merged/Assets/Scripts/CombatDummyController.cs
+++ 2D Platformer Merged/Assets/Scripts/CombatDummyController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CombatDummyController : MonoBehaviour {
-
     [SerializeField]
     private float maxHealth, knockbackSpeedX, knockbackSpeedY, knockbackDuration, knockbackDeathSpeedX, knockbackDeathSpeedY, deathTorque; 
     [SerializeField]
