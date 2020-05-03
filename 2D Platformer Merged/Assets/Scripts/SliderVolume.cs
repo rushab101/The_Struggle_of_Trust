b1f40807d21973c7534eaac67f50d2fbb26c2505
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-05-03 17:19:09 -0400

    Done Script for Sound
    +Sound Script
    +Added Main Menu Audio
    +Version 0.1 Added

---
+++ 2D Platformer Merged/Assets/Scripts/SliderVolume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderVolume : MonoBehaviour
+{
+    private AudioSource AudioSrc;
+
+    private float AudioVolume = 1f;
+
+    void Start()
+    {
+        AudioSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        AudioSrc.volume = AudioVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        AudioVolume = vol;
+    }
+}
