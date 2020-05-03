07c0c08540554696915e3ee207cbe8b50b68680d
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-05-03 06:00:27 -0400

    Added Main Menu
    Main Menu Complete, but option slider script not implemented.

---
+++ 2D Platformer Merged/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string level;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(level);
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+
+}
