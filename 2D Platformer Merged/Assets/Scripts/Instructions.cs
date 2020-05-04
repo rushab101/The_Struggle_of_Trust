6270a958c96b8f2db9ca1102bc0e13ee27f11ca7
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-05-04 04:24:42 -0400

    Added an instruction screen
    Need to take out instruction screen after the first time of playing

---
+++ 2D Platformer Merged/Assets/Scripts/Instructions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+    public GameObject startUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startUI.SetActive(true);
+        Time.timeScale = 0f;
+
+
+    }
+
+    public void Continue()
+    {
+        startUI.SetActive(false);
+        Time.timeScale = 1f;
+
+    }
+
+    
+}
