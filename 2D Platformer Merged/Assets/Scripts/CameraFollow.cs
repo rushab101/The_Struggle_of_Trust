03829b21f8167541f718f88de5220cc962c42a93
Uddin-Humaiyun-215492994 <46493951+Uddin-Humaiyun-215492994@users.noreply.github.com>
2020-05-06 22:26:13 -0400

    Added camera bounds, size, and behaviour
    - camerafollow script is not needed; all camera movement is done in cinemachine instead, since i forgot thats a thing stupid me >:(

---
+++ 2D Platformer Merged/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Vector2 velocity;
+
+    public float smoothTimeY, smoothTimeX;
+
+    public GameObject Player;
+
+    public bool bounds;
+
+    public Vector3 minCameraPosition;
+    public Vector3 maxCameraPosition;
+
+
+    void Start() {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate() {
+        float positionX = Mathf.SmoothDamp(transform.position.x, Player.transform.position.x, ref velocity.x, smoothTimeX);
+        float positionY = Mathf.SmoothDamp(transform.position.y, Player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(positionX, positionY, transform.position.z);
+
+        if (bounds) {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPosition.x, maxCameraPosition.x),
+                Mathf.Clamp(transform.position.y, minCameraPosition.y, maxCameraPosition.y),
+                Mathf.Clamp(transform.position.z, minCameraPosition.z, maxCameraPosition.z));
+        }
+    }
+}
