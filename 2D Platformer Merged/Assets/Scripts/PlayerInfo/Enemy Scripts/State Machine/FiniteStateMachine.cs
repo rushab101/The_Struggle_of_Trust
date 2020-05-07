928237140236297cda18ce2d1d4da64d4f79c777
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-05-07 06:45:05 -0400

    Done Part 1 of Advanced AI
    the wolf now idle-> move and move-> idle using save objects instead of one single script

---
+++ 2D Platformer Merged/Assets/Scripts/PlayerInfo/Enemy Scripts/State Machine/FiniteStateMachine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiniteStateMachine 
+{
+    public State currentState {get; private set; } //getter -> any class function can get it and setter ->only this class can set the functions
+    public void Initialize(State startingState)
+    {
+        currentState= startingState;
+        currentState.Enter();
+    }
+
+    public void ChangeState(State newState)
+    {
+        currentState.Exit();
+        currentState = newState;
+        currentState.Enter();
+        
+    }
+    
+}
---
+++ 2D Platformer Merged/Assets/Scripts/PlayerInfo/Enemy Scripts/States/Data/D_MoveState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="newMoveStateData",menuName = "Data/State Data/Move State")]
+public class D_MoveState : ScriptableObject
+{
+  public float movementSpeed = 3f;
+  
+
+
+
+
+
+}
