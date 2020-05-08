53874e8e815d8593d381693ba914733113e6e69d
Uddin-Humaiyun-215492994 <46493951+Uddin-Humaiyun-215492994@users.noreply.github.com>
2020-05-08 13:11:35 -0400

    created hurtstate script

---
+++ 2D Platformer Merged/Assets/Scripts/PlayerInfo/Enemy Scripts/States/HurtState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtState : State
+{
+    public HurtState(Entity etity, FiniteStateMachine stateMachine, string animBoolName) : base(etity, stateMachine, animBoolName) {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
