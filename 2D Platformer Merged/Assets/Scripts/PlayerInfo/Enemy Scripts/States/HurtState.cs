946aa7b4f2a1ebe2b7c913269ae62910f3a02465
Uddin-Humaiyun-215492994 <46493951+Uddin-Humaiyun-215492994@users.noreply.github.com>
2020-05-08 13:14:11 -0400

    generate overrides

--- 2D Platformer Merged/Assets/Scripts/PlayerInfo/Enemy Scripts/States/HurtState.cs
+++ 2D Platformer Merged/Assets/Scripts/PlayerInfo/Enemy Scripts/States/HurtState.cs
@@ -7,6 +7,38 @@ public class HurtState : State
     public HurtState(Entity etity, FiniteStateMachine stateMachine, string animBoolName) : base(etity, stateMachine, animBoolName) {
     }
 
+    public override void DoChecks() {
+        base.DoChecks();
+    }
+
+    public override void Enter() {
+        base.Enter();
+    }
+
+    public override bool Equals(object obj) {
+        return base.Equals(obj);
+    }
+
+    public override void Exit() {
+        base.Exit();
+    }
+
+    public override int GetHashCode() {
+        return base.GetHashCode();
+    }
+
+    public override void LogicUpdate() {
+        base.LogicUpdate();
+    }
+
+    public override void PhysicsUpdate() {
+        base.PhysicsUpdate();
+    }
+
+    public override string ToString() {
+        return base.ToString();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
