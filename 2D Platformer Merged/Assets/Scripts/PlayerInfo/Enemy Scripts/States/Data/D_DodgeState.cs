570d0aa00fe82be98732df0083569c61825d39aa
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-05-12 15:49:38 -0400

    Some simple Changes

---
+++ 2D Platformer Merged/Assets/Scripts/PlayerInfo/Enemy Scripts/States/Data/D_DodgeState.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="newDodgeStateData", menuName = "Data/State Data/Dodge State")]
+public class D_DodgeState : ScriptableObject
+{
+    
+}
---
+++ 2D Platformer Merged/Assets/Scripts/PlayerInfo/Enemy Scripts/States/DodgeState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeState : State
+{
+    protected D_DodgeState stateData;
+    public DodgeState(Entity etity, FiniteStateMachine stateMachine, string animBoolName, D_DodgeState stateData) : base(etity, stateMachine, animBoolName)
+    {
+        this.stateData = stateData;
+    }
+
+       public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        
+    }
+
+  
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
