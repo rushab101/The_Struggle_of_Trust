fb70ade06d02d1e9af5093776101ad75c80e7e9a
Uddin-Humaiyun-215492994 <46493951+Uddin-Humaiyun-215492994@users.noreply.github.com>
2020-05-26 15:18:39 -0400

    Update AudioManager.cs

--- 2D Platformer Merged/Assets/Scripts/Audio/AudioManager.cs
+++ 2D Platformer Merged/Assets/Scripts/Audio/AudioManager.cs
@@ -6,7 +6,20 @@ public class AudioManager : MonoBehaviour {
 
     public Sound[] sounds;
 
+    public static AudioManager instance;
+
     void Awake() {
+
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
         foreach (Sound s in sounds) {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
@@ -19,5 +32,6 @@ public class AudioManager : MonoBehaviour {
     public void Play(string name) {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
+        // Adding audio source in other scripts e.g. running => FindObjectOfType<AudioManager>().Play(Name);
     }
 }
