637de184aee069cab7d48cb6d4e0305e3ee86ffb
Uddin-Humaiyun-215492994 <46493951+Uddin-Humaiyun-215492994@users.noreply.github.com>
2020-05-26 14:35:59 -0400

    Name

---
+++ 2D Platformer Merged/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    void Awake() {
+        foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
---
+++ 2D Platformer Merged/Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range (.1f, 3f)]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+
+}
