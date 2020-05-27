57844d6846013f417c6f5b16bd7f6423f6886802
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-05-27 17:30:37 -0400

    Added FireSymbolDone

---
+++ 2D Platformer Merged/Assets/Scripts/Core Gameplay/Tutorial/SomerSault.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomerSault : MonoBehaviour
+{
+
+    public GameObject canvas;
+    bool first = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+        
+    }
+
+     private void OnTriggerEnter2D(Collider2D collision) 
+    {
+      
+         if (collision.CompareTag("Player") && !first)
+        {
+                canvas.SetActive(true);
+              // first=true;
+                 
+        }
+  
+
+    }
+
+    private void OnTriggerExit2D( Collider2D collision ) 
+    {
+       canvas.SetActive(false);
+       
+    }
+
+    // Update is called once per frame
+   
+}
