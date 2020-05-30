8928fa7b70bc9ecb5871b1e4b6ee1f305ad53e98
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-05-30 17:17:00 -0400

    Added Staglites

---
+++ 2D Platformer Merged/Assets/Scripts/Hazards/StagLite.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagLite : MonoBehaviour
+{
+  
+
+    public float FallSpeed = 0.1f;
+    [SerializeField]
+   private Vector3 velocity;
+  
+    public float ySetYGoal;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if ( FindObjectOfType<Trigger>().ApplyVol)
+        {
+            if (transform.localPosition.y >= ySetYGoal)
+            {
+                //Debug.Log(transform.position.y);
+                transform.localPosition +=(velocity * Time.deltaTime);
+            }
+            else 
+            {
+           //     Debug.Log("IN HERE");
+                transform.localPosition +=(Vector3.zero * Time.deltaTime);
+            }
+            
+        }
+        
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision) {
+       
+     
+
+           
+              
+            
+   
+       
+
+
+        if (collision.CompareTag("Player") &&   !FindObjectOfType<Trigger>().noDamage) {
+
+            FindObjectOfType<TimeStop>().StopTime(0.05f, 10, 0.1f);
+           // UnityEngine.Debug.Log("Timestop TRUE (Spikes)");
+
+            FindObjectOfType<PlayerStats>().TakeDamage(1f);
+           // UnityEngine.Debug.Log("Health-- TRUE (Spikes)");
+
+            FindObjectOfType<PlayerController>().knockBack(FindObjectOfType<PlayerController>().GetFacingDirection());
+          //  UnityEngine.Debug.Log("Knockback TRUE (Spikes)");
+        
+    }
+    }
+
+
+
+}
