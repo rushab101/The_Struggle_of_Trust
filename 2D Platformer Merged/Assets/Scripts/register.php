09e4aa9e34f8cd10d640f7101967867b670f2cc1
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-11-29 21:06:06 -0500

    Added Yeti AI

--- 2D Platformer Merged/Assets/Scripts/register.php
+++ 2D Platformer Merged/Assets/Scripts/register.php
@@ -1,4 +1,11 @@
 <?php
-	#con = mysqli_connect('localhost','root','root');
+	$con = mysqli_connect('localhost','root','root','UnitySQL');
+
+	if (mysqli_connect_errorno()
+	{
+		echo "1":
+		exit();
+	} 
+	
 
 ?> 
