fecc7f3b43fcc5a408846f925271fde82b4230fc
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-12-02 01:59:27 -0500

    Added Code Part

---
+++ 2D Platformer Merged/Assets/EndTrigger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class EndTrigger : MonoBehaviour
+{
+     public GameObject enter_code_menu;
+     private InputField input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = enter_code_menu.GetComponent<InputField>();
+       
+
+        enter_code_menu.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    private void OnTriggerStay2D(Collider2D collision) {
+       
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            EnterMenu();
+        }
+
+    }
+
+
+
+    void EnterMenu()
+    {
+        enter_code_menu.SetActive(true);
+         Time.timeScale = 0f;
+        input.onEndEdit.AddListener(delegate { inputBetValue(input); });
+
+
+
+    }
+
+      public  void Resume()
+    {
+        enter_code_menu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+public void inputBetValue(InputField userInput)
+{
+    
+    Debug.Log(userInput.text);
+}
+
+}
