ec628be09c8f9cc45c9429d808b5030246d87e52
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-12-08 21:07:55 -0500

    Addeed More Fine Tuning

---
+++ 2D Platformer Merged/Assets/PendulumSFX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumSFX : MonoBehaviour
+{
+   public AudioSource audio;
+
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.CompareTag("Player"))
+        {
+            
+             audio.Play();
+
+            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+          if (collision.CompareTag("Player"))
+        {
+          audio.Pause();
+        }
+       
+    }
+
+
+
+
+}
