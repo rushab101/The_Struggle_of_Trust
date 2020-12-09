2fe78b2cbc624e08b9f6214aa50c1841f4f6a22c
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-12-08 19:45:56 -0500

    Fixed Some Bugs

--- 2D Platformer Merged/Assets/CounterScript.cs
+++ 2D Platformer Merged/Assets/CounterScript.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class CounterScript : MonoBehaviour
 {
     public int counter = 0;
-        private void OnTriggerStay2D(Collider2D collision) 
+    private void OnTriggerStay2D(Collider2D collision)
     {
-//        Debug.Log("Went into trigger");
-        if (Input.GetKeyDown(KeyCode.Z))
+        //        Debug.Log("Went into trigger");
+        if (collision.CompareTag("Player"))
         {
-               counter++;
-        }      
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                counter++;
+                 FindObjectOfType<AudioManager>().Play("break"); // 06 June 2020
+
+            }
+
+        }
+
     }
 }
