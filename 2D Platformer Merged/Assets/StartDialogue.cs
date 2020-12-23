75eab0ec15fa4cc8475565cb211ef8e967eb4d33
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-12-22 22:26:26 -0500

    Intro Cut Scene

---
+++ 2D Platformer Merged/Assets/StartDialogue.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartDialogue : MonoBehaviour
+{
+    public GameObject canvasObject;
+    private bool firstSentence = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E) && !firstSentence)
+        {
+            canvasObject.SetActive(true);
+            firstSentence = true;
+             FindObjectOfType<IntroCutSceneWorkingDialogue>().TriggerDialogue();
+
+        }
+        else if (firstSentence && Input.GetKeyDown(KeyCode.E)) 
+        {
+           
+
+            FindObjectOfType<DialogueManagerIntro>().DisplayNextSentence();
+            if (canvasObject.activeSelf && FindObjectOfType<DialogueManagerIntro>().sentences.Count != 0)
+                //FindObjectOfType<AudioManager>().Play("Render_Text"); // 06 June 2020
+                if (FindObjectOfType<DialogueManagerIntro>().sentences.Count == 0)
+                {
+                    canvasObject.SetActive(false);
+                    SceneManager.LoadScene("Beginning");
+                }
+        }
+
+
+
+    }
+
+
+
+   
+}
