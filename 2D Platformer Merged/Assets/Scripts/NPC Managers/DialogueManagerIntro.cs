463ebdb641316acf7cb122b53872e201a0e2caa1
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2020-12-28 22:15:12 -0500

    Update DialogueManagerIntro.cs

--- 2D Platformer Merged/Assets/Scripts/NPC Managers/DialogueManagerIntro.cs
+++ 2D Platformer Merged/Assets/Scripts/NPC Managers/DialogueManagerIntro.cs
@@ -68,6 +68,9 @@ public class DialogueManagerIntro : MonoBehaviour
 			yield return null;
 			yield return null;
 			yield return null;
+			yield return null;
+			yield return null;
+			yield return null;
 			
 		}
 	}
